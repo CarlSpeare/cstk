e9d41f513f82cec7ca1f770d004b1175369c5291
Carl Speare <carlds@panix.com>
2021-05-27 21:32:09 -0400

    minor tweaks on shmtest.c

--- src/shmtest.c
+++ src/shmtest.c
@@ -1,4 +1,5 @@
 /* check shmget's ability to allocate large shm segs */
+/* Note that this is mostly geared toward Linux, but should in theory work on POSIX platforms. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
@@ -9,7 +10,7 @@ int main(void)
         long int shmid;
         long int *shmptr;
         unsigned long int maxsize = 0;
-        printf ("Please enter the shared mem segment size to allocate: ");
+        printf ("Please enter the shared mem segment size to allocate (in bytes, integer): ");
         scanf ("%ld", &maxsize);
         shmid = shmget (IPC_PRIVATE, maxsize, (IPC_CREAT | 0666));
         if ( shmid == -1 ) {
@@ -22,7 +23,7 @@ int main(void)
                 exit (2);
         }
         printf ("Allocated %ld bytes on segment id %d\n\n", maxsize, shmid);
-        printf ("Waiting for you to enter a number and press enter to exit... ");
+        printf ("Waiting for you to enter an integer and press enter to release shared memory and exit... ");
         scanf ("%ld", &maxsize);
         shmdt (shmptr);
         shmctl (shmid, IPC_RMID, 0);
